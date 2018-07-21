8e38d5719ef97aa794ac5a1626311ad044d6e492
lizhen <lizhn95@163.com>
2018-07-21 15:24:23 +0800

    init

---
+++ src/test/java/com/zhenli/concurent/ConcurentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhenli.concurent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
